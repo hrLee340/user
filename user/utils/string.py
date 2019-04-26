b7b05e63d4b71b09b22524fa2699a1a0435997c2
haoranLee2017 <1013697949@qq.com>
2019-04-26 16:45:05 +0800

    string

---
+++ user/utils/string.py
@@ -0,0 +1,57 @@
+# print("what's your name")
+# print('what\'s your name')
+#
+#
+# var = "hello python"
+# print(len(var))
+#
+#
+# var1 = "hello"
+# var2 = "python"
+# var3 = var1 + var2
+#
+# print(" ".join(["这些字符串有：", var1, var2]))
+
+# l = ["hello", "python"]
+# print(" ".join(l))
+
+
+# print("I like {0} and {1}".format("study", "basketball"))
+
+
+# s = "a,b,c,d,e,f"
+# print(s.split(","))
+
+
+# 三引号举例
+# s = """ "how" are you ?
+#  I'm fine, thank you, and you?
+#  me too
+# """
+# print(s)
+#
+#
+# # 转义符使用
+# s2 = "how \"are\" you ?"
+# print(s2)
+#
+#
+# # 从键盘输入你的名字
+# name = input("what is your name ?")
+# print("my name is " + name)
+
+
+
+
+# s = """ hello python
+#          hello world
+#
+# """
+# print(s)
+
+# lis = [i for i in range(10) if i == 1]
+# print(lis)
+
+
+# a = "abcde"
+# print('-'.join(a))
